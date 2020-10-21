037f53f20ac702b76601aee56433f883f43fbc5c
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-21 21:50:38 +0000

    Initalize methods to CLI class

---
+++ environment.rb
@@ -0,0 +1,7 @@
+require_relative './lib/API.rb'
+require_relative './lib/CLI.rb'
+require_relative './lib/Films.rb'
+
+require 'pry'
+require 'net/http' #makes requests from API
+require 'json' #parses JSON from API
---
+++ lib/CLI.rb
@@ -0,0 +1,10 @@
+class CLI
+
+  def initialize
+  end
+
+  def start
+    puts "Welcome!"
+  end
+
+end
