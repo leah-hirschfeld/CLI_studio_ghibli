b4f5cc03e82234efb5524bc9877cfa46973b7d36
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-22 02:20:33 +0000

    Add numbers to list of films

--- lib/API.rb
+++ lib/API.rb
@@ -10,7 +10,10 @@ class API
     response = Net::HTTP.get(uri)
     array = JSON.parse(response)
 
+    counter = 0
+
     array.each do |film_hash|
+      counter += 1
       film = Film.new
       film.title = film_hash["title"]
       film.director = film_hash["director"]
@@ -18,6 +21,8 @@ class API
       film.release_date = film_hash["release_date"]
       film.description = film_hash["description"]
       film.rt_score = film_hash["rt_score"]
+
+      puts "#{counter} #{film.title}"
     end
 
   end
